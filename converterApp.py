50dc8179ff876f268caf2d29d62f08043b9b58d5
Sam Jacks <sammmichaeljacks@gmail.com>
2023-01-24 22:51:44 +0000

    cleaned up

--- converterApp.py
+++ converterApp.py
@@ -1,26 +1,46 @@
-value = []
-flag = ""
-for number in nums:
-    flag += chr(number)
-print(flag)
+import re
 
+def DecimalToText():
+    inputs = input("Please give values, needs to be in a space seperated list\n").split()
+    output = ''
+    for i in inputs:
+        output += chr(int(i))
+    print(output)
 
-flag = 'ocip{FTC0l_I4_t5m_ll0m_y_y3n58a025e3ÿ~R}'
+def AsciiToText():
+    inputs = input("Please give values, needs to be in a space seperated list\n").split()
+    output = []
+    for i in inputs:
+        cleansedInput = re.sub('[a-zA-Z]', '', i)
+        output.append(chr(int(cleansedInp)))
+    print(''.join(output))
 
-blocks = []
-for i in range(0, len(flag), 4):
-    chunk = flag[i::]
-    if len(chunk) > 4:
-        chunk = chunk[0:4]
-    blocks.append(''.join(chunk))
+def TextFlipper():
+    inputs = input("Please give input to switch around\n")
+    listOutput = []
+    for i in range(0, len(inputs), 4):
+        byte = inputs[i:(i+4)]
+        listOutput.append(''.join(byte))
+    textOutput = ''
+    for i in reversed(listOutput):
+        textOutput = textOutput + i[::-1]
+    print(textOutput)
 
-output = ''
-for i in blocks:
-    output = output + i[::-1]
+def main():
+    action = input("What do you want to complete:\n"
+    +"1. Convert Decimal to Text\n"
+    +"2. Convert Ascii to Text\n"
+    +"3. Flip some Text\n")
 
-print(output)
+    match action:
+        case "1":
+            DecimalToText()
+        case "2":
+            AsciiToText()
+        case "3":
+            TextFlipper()
+        case _:
+            quit()
 
-
-ist = [chr(0x62),chr(0x64),chr(0x61),chr(0x36),chr(0x38),chr(0x66),chr(0x37),chr(0x35)]
-for i in list:
-    print(i)
+if __name__ == "__main__":
+    main()
